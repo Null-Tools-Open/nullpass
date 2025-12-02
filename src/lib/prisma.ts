36949084d693dc8eace8eee722da6b2256fd2d9d
lis0ownik <kkowalewski844@gmail.com>
2025-12-02 21:23:37 +0100

    add two libs to allow for seamless migration of all users

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -7,3 +8,20 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+let dropDbPool: Pool | null = null
+
+export function getDropDbPool(): Pool {
+  if (!process.env.DROP_DATABASE_URL) {
+    throw new Error('DROP_DATABASE_URL is not set')
+  }
+
+  if (!dropDbPool) {
+    dropDbPool = new Pool({
+      connectionString: process.env.DROP_DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  }
+
+  return dropDbPool
+}
